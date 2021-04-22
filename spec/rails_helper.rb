29378fa89237aa6c85035b3074e7bb9b783fbe82
MarshallAllshouseUCCS <mallshou@uccs.edu>
2021-04-22 00:55:29 +0000

    added SimpleCov filters

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,11 @@
 require 'simplecov'
-SimpleCov.start
+SimpleCov.start 'rails' do
+  add_filter '/bin/'
+  add_filter '/db/'
+  add_filter '/spec/'
+  add_filter '/log/'
+  add_filter '/coverage/'
+end
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
